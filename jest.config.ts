ffdce71f899b26afd9e5d6a6dc10ec8b642d6511
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-05-07 11:06:45 -0400

    chore: add jestConfig

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>src/**/*.ts'],
+  coverageDirectory: "coverage",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  "transform": {
+    '.+\\.ts$':'ts-jest'
+  }
+};
