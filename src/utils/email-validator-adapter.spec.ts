a248b0faa4350ef812754d6aca35dbb2e13a2e93
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-05-26 18:01:02 -0400

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,27 @@
 import {EmailValidatorAdapter} from "./email-vaidator";
+import validator from "validator";
+import {isBooleanObject} from "node:util/types";
+
+jest.mock('validator', () => ({
+    isEmail(): boolean {
+        return true;
+    }
+}));
 
 describe('EmailValidator Adapter', () => {
     it('should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter();
-        sut.isValid = sut.isValid('invalid_email@mil.com');
-        expect(sut.isValid).toBe(false);
+        jest.spyOn(validator, 'isEmail').mockImplementationOnce(() => {
+            return false;
+        });
+
+        const isValid = sut.isValid('invalid_email@mil.com');
+        expect(isValid).toBe(false);
+    });
+
+    it('should return true if validator returns true', () => {
+        const sut = new EmailValidatorAdapter();
+        const isValid = sut.isValid('valid_email@mil.com');
+        expect(isValid).toBe(true);
     });
 });
