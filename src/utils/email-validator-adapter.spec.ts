2f385bbb0c00b7d34986d5d258d18ccdfe315143
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-05-26 18:22:38 -0400

    refactor: move su creation ta factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -8,9 +8,13 @@ jest.mock('validator', () => ({
     }
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter();
+}
+
 describe('EmailValidator Adapter', () => {
     it('should return false if validator returns false', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         jest.spyOn(validator, 'isEmail').mockImplementationOnce(() => {
             return false;
         });
@@ -20,13 +24,13 @@ describe('EmailValidator Adapter', () => {
     });
 
     it('should return true if validator returns true', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isValid = sut.isValid('valid_email@mil.com');
         expect(isValid).toBe(true);
     });
 
     it('should call validator with correct email', () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isEmailSpy = jest.spyOn(validator, 'isEmail');
         sut.isValid('any_email@mail.com');
         expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
