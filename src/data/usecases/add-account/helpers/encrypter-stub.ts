6b2ad3796d2ac46359fc0182876e64e6d5e6cc8c
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-06-19 19:19:20 -0300

    refactor: make encrypterStub helper implements the Encrypter class

--- src/data/usecases/add-account/helpers/encrypter-stub.ts
+++ src/data/usecases/add-account/helpers/encrypter-stub.ts
@@ -1,5 +1,7 @@
-export class EncrypterStub {
-    async encrypt(value: string): Promise<string> {
+import {Encrypter} from "../../../protocols/encrypter";
+
+export class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
         return new Promise((resolve) => resolve("hashed_password"));
     }
 }
