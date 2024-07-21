638bb9bf76ec8e4e98a2e26884db26d2a9cd4c84
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-07-21 10:57:04 -0400

    feat: ensure bcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -9,4 +9,14 @@ describe('Bcrypt Adapter', () => {
         await sut.encrypt('any_value');
         expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
     });
+
+    it('should return a hash on success', async () => {
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+            return Promise.resolve('hash');
+        })
+        const salt = 12;
+        const sut = new BcryptAdapter(salt);
+        const hash = await sut.encrypt('any_value');
+        expect(hash).toBe('hash');
+    });
 });
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -5,7 +5,6 @@ export class BcryptAdapter implements Encrypter {
     constructor(private salt: number) {}
 
     async encrypt(value: string): Promise<string> {
-        await bcrypt.hash(value, this.salt);
-        return ''
+        return await bcrypt.hash(value, this.salt);
     }
 }
